b6207c0b1edd764678d49cfd2ece67cbb132d90a
Tung Phan <thanhtungphanvu@gmail.com>
2023-06-04 20:35:21 +0300

    .

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 import Card from "../UI/Card";
 const Expenses = (props) => {
@@ -16,6 +17,7 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onYearSave={selectedYear} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
