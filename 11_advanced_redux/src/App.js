34123b425ef2bb614e0aa69e8314ac0332981ced
Tung Phan <thanhtungphanvu@gmail.com>
2023-08-23 21:57:08 +0300

    save cart data

--- 11_advanced_redux/src/App.js
+++ 11_advanced_redux/src/App.js
@@ -1,13 +1,24 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isShow = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-1afea-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
-      { isShow && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
