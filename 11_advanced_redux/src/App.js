4a00ca3e2b8a8ca2ddd5e5e8a24d61d9763ca608
Tung Phan <thanhtungphanvu@gmail.com>
2023-08-31 01:45:32 +0300

    fix and update database logic

--- 11_advanced_redux/src/App.js
+++ 11_advanced_redux/src/App.js
@@ -1,12 +1,11 @@
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { Fragment, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { sendCartData } from './store/cart-slice';
-import Notification from './components/UI/Notification';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
+import Notification from "./components/UI/Notification";
 
 let isInitial = true;
 
@@ -15,13 +14,18 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+    }
+    
   }, [cart, dispatch]);
 
   return (
