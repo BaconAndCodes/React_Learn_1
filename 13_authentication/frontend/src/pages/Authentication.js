4705c813aae8c5b06df64b330b278ff40e66ccf3
Tung Phan <thanhtungphanvu@gmail.com>
2023-10-26 05:30:33 +0300

    update auth token

--- 13_authentication/frontend/src/pages/Authentication.js
+++ 13_authentication/frontend/src/pages/Authentication.js
@@ -1,40 +1,48 @@
-import AuthForm from "../components/AuthForm";
 import { json, redirect } from "react-router-dom";
+
+import AuthForm from "../components/AuthForm";
+
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
-export const action = async ({ request }) => {
+
+export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode") || "login";
 
   if (mode !== "login" && mode !== "signup") {
-    throw json(
-      {
-        message: "Unsupported mode.",
-      },
-      { status: 422 }
-    );
+    throw json({ message: "Unsupported mode." }, { status: 422 });
   }
+
   const data = await request.formData();
   const authData = {
     email: data.get("email"),
     password: data.get("password"),
   };
 
-  const response = await fetch("http://localhost:8080/login" + mode, {
+  const response = await fetch("http://localhost:8080/" + mode, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(authData),
   });
-  if (response.stats === 422 || response.stats === 401) {
+
+  if (response.status === 422 || response.status === 401) {
     return response;
   }
+
   if (!response.ok) {
-    throw json({ message: "Could not authentication user." }, { status: 500 });
+    throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
-  return redirect('/')
-};
+  const resData = await response.json();
+  const token = resData.token;
+  // soon: manage that token
+
+
+  localStorage.setItem("token", token);
+  
+  return redirect("/");
+}
