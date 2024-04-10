a72669a50e7bbc57ef41b6dbe5e5d91228527ae7
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-11 01:45:12 +0300

    add save meal to the database and error message

--- 15.1_next-js-meal/components/meals/image-picker.js
+++ 15.1_next-js-meal/components/meals/image-picker.js
@@ -1,15 +1,37 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 import classes from "./image-picker.module.css";
 const ImagePicker = ({ label, name }) => {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
+
   const handlePickClick = () => {
     imageInput.current.click();
+  };
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   };
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="the image selected by user" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -17,6 +39,8 @@ const ImagePicker = ({ label, name }) => {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
---
+++ 15.1_next-js-meal/components/meals/meals-form-submit.js
@@ -0,0 +1,7 @@
+"use client";
+import { useFormStatus } from "react-dom";
+const MealsFormSubmit = () => {
+  const { pending } = useFormStatus();
+  return <button>{pending ? "Submitting..." : "Share Meal"}</button>;
+};
+export default MealsFormSubmit;
