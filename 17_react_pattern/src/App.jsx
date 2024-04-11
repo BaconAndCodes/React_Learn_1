b4d5869dd156837e6bec4a1e9c1916d540f926de
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-11 16:23:41 +0300

    add accordion

--- 17_react_pattern/src/App.jsx
+++ 17_react_pattern/src/App.jsx
@@ -1,5 +1,34 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us? </h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            title="We got 20 years of experience"
+          >
+            <article>
+              <p>you cant go wrong with us</p>
+              <p>More than 20 years</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            title="We work with local guys"
+          >
+            <article>
+              <p>you cant go wrong with us</p>
+              <p>More than 20 years</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
